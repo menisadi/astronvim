94406005b624158c6d51dd6de51712841aef3956
sadigu <menis@intentiq.com>
2024-04-04 12:16:02 +0300

    adding little keymap for lazyvim stats

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -9,11 +9,11 @@ return {
     -- Configure core features of AstroNvim
     features = {
       large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      autopairs = true,                                 -- enable autopairs at start
+      cmp = true,                                       -- enable completion at start
+      diagnostics_mode = 3,                             -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      highlighturl = true,                              -- highlight URLs at start
+      notifications = true,                             -- enable notifications at start
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
@@ -22,12 +22,12 @@ return {
     },
     -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
+      opt = {                  -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = true, -- sets vim.opt.spell
-        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-        wrap = false, -- sets vim.opt.wrap
+        number = true,         -- sets vim.opt.number
+        spell = true,          -- sets vim.opt.spell
+        signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
+        wrap = false,          -- sets vim.opt.wrap
         cursorcolumn = true,
       },
       g = { -- vim.g.<key>
@@ -48,6 +48,15 @@ return {
         --   desc = "Flash jump",
         --   remap = true,
         -- },
+        ["<Leader>Sz"] = {
+          function()
+            local stats = require("lazy").stats()
+            local ms = math.floor(require("lazy").stats().startuptime * 100 + 0.5) / 100
+            local stat_string = "loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms"
+            vim.api.nvim_echo({ { stat_string, "Title" } }, true, {})
+          end,
+          desc = "Show Lazy loading stats",
+        },
         ["<Leader>lc"] = {
           function() require("neogen").generate() end,
           noremap = true,
