fca12ddcc43b1aaa0fa3b977bc81a73cd53686e8
sadigu <menis@intentiq.com>
2024-04-10 21:40:58 +0300

    fixed alpha screen

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,10 +3,10 @@ return {
   opts = function(_, opts)
     opts.section.buttons.val = {
       opts.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-      opts.button("SPC O", " Oil", "<CMD> Oil <CR>"),
-      opts.button("SPC f f", "󰐱 Find file", "<CMD> Telescope find_files <CR>"),
-      opts.button("SPC f z", " Zoxide", "<CMD> Telescope zoxide list <CR>"),
-      opts.button("SPC f o", "󰋚 Recentls", "<CMD> Telescope oldfiles <CR>"),
+      opts.button("SPC O", "  Oil", "<CMD> Oil <CR>"),
+      opts.button("SPC f f", "󰐱  Find file", "<CMD> Telescope find_files <CR>"),
+      opts.button("SPC f z", "  Zoxide", "<CMD> Telescope zoxide list <CR>"),
+      opts.button("SPC f o", "󰋚  Recents", "<CMD> Telescope oldfiles <CR>"),
       -- opts.button("SPC S l", "󰕇 Last session", ":echo not working yet <CR>"),
       opts.button("q", "󰅚  Quit", ":qa<CR>"),
     }
