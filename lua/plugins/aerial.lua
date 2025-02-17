ce731e124b574180ad9e79dd17d779a66bf2a4bf
sadigu <menis@intentiq.com>
2025-02-17 16:37:43 +0200

    removed dressing import
    not sure why it was there to begin with

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,22 +1,12 @@
 return {
-  {
-    "stevearc/aerial.nvim",
-    opts = {
-      filter_kind = {
-        markdown = false,
-        ["_"] = {
-          "Class",
-          "Function",
-          "Method",
-        },
-      },
-    },
-  },
-  {
-    "dressing.nvim",
-    opts = {
-      input = {
-        insert_only = false,
+  "stevearc/aerial.nvim",
+  opts = {
+    filter_kind = {
+      markdown = false,
+      ["_"] = {
+        "Class",
+        "Function",
+        "Method",
       },
     },
   },
