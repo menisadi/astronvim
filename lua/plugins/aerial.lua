bc80256dacb7b6ed819e61969ee2ac1cb4893b11
sadigu <menis@intentiq.com>
2025-06-04 08:13:54 +0300

    don't show enums
    it's just too much

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,28 @@
+require("aerial").setup({
+  filter_kind = {
+    "Class",
+    "Constructor",
+    "Enum",
+    "Function",
+    "Interface",
+    "Module",
+    "Method",
+    "Struct",
+  },
+})
+
+return {
+  "stevearc/aerial.nvim",
+  opts = {
+    filter_kind = {
+      "Class",
+      "Constructor",
+      -- "Enum",
+      "Function",
+      "Interface",
+      "Module",
+      "Method",
+      "Struct",
+    },
+  },
+}
