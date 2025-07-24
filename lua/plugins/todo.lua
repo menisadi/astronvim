3f6346423f26cc32694973c9ee3955a20267ed1f
sadigu <menis@intentiq.com>
2025-07-24 07:43:38 +0300

    added 'temp' comment time

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,15 +1,21 @@
 return {
-  "folke/todo-comments.nvim",
-  opts = {
-    keywords = {
-      QSTN = {
-        icon = "? ",
-        color = "wonder",
-        alt = { "ASK", "WONDER", "QUES", "THINK" },
-      },
-    },
-    colors = {
-      wonder = { "#9966CC" },
-    },
-  },
+	"folke/todo-comments.nvim",
+	opts = {
+		keywords = {
+			TEMP = {
+				icon = " ",
+				color = "time",
+				alt = { "TEMPORARY", "TMP", "WIP" },
+			},
+			QSTN = {
+				icon = "? ",
+				color = "wonder",
+				alt = { "ASK", "WONDER", "QUES", "THINK" },
+			},
+		},
+		colors = {
+			wonder = { "#9966CC" },
+			time = { "#9CA3AF" },
+		},
+	},
 }
