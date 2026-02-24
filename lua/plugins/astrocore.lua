232df0a92b58c7c603dcc861a0ca969d04b7b1f0
sadigu <menis@intentiq.com>
2026-02-24 09:24:22 +0200

    Added light/dark autoswitch
    Also moved the mappings to astrocore.lua

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,84 +1,89 @@
-if true then
-	return {}
-end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
--- Configuration documentation can be found with `:h astrocore`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
 ---@type LazySpec
 return {
 	"AstroNvim/astrocore",
 	---@type AstroCoreOpts
 	opts = {
-		-- Configure core features of AstroNvim
-		features = {
-			large_buf = { size = 1024 * 256, lines = 10000 },          -- set global limits for large files for disabling features like treesitter
-			autopairs = true,                                          -- enable autopairs at start
-			cmp = true,                                                -- enable completion at start
-			diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-			highlighturl = true,                                       -- highlight URLs at start
-			notifications = true,                                      -- enable notifications at start
+		autocmds = {
+			colorscheme_background = {
+				{
+					event = "OptionSet",
+					pattern = "background",
+					desc = "Switch colorscheme when background changes",
+					callback = function()
+						if vim.o.background == "dark" then
+							vim.cmd.colorscheme("kanagawa")
+						else
+							vim.cmd.colorscheme("default")
+						end
+					end,
+				},
+			},
 		},
-		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-		diagnostics = {
-			virtual_text = true,
-			underline = true,
-		},
-		-- vim options can be configured here
 		options = {
-			opt = {              -- vim.opt.<key>
-				relativenumber = true, -- sets vim.opt.relativenumber
-				number = true,     -- sets vim.opt.number
-				spell = false,     -- sets vim.opt.spell
-				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-				wrap = false,      -- sets vim.opt.wrap
-			},
-			g = {                -- vim.g.<key>
-				-- configure global vim variables (vim.g)
-				-- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-				-- This can be found in the `lua/lazy_setup.lua` file
+			g = {
+				python3_host_prog = "/Users/meni/.pyenv/versions/nvim/bin/python3",
 			},
 		},
-		-- Mappings can be configured through AstroCore as well.
-		-- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
 		mappings = {
-			-- first key is the mode
 			n = {
-				-- second key is the lefthand side of the map
-
-				-- navigate buffer tabs
-				["]b"] = {
-					function()
-						require("astrocore.buffer").nav(vim.v.count1)
-					end,
-					desc = "Next buffer",
+				["<Leader>lq"] = {
+					":!sqlfluff fix %<CR><CR>",
+					desc = "Run SqlFluff on the current buffer.",
+					noremap = true,
+					silent = true,
 				},
-				["[b"] = {
-					function()
-						require("astrocore.buffer").nav(-vim.v.count1)
-					end,
-					desc = "Previous buffer",
+				["<Leader>ra"] = {
+					":SlimeSend1 %autoindent<CR>",
+					desc = "Toggle autoindent on IPython REPL",
+					noremap = true,
+					silent = true,
 				},
-
-				-- mappings seen under group name "Buffer"
-				["<Leader>bd"] = {
-					function()
-						require("astroui.status.heirline").buffer_picker(function(bufnr)
-							require("astrocore.buffer").close(bufnr)
-						end)
-					end,
-					desc = "Close buffer from tabline",
+				["<Leader>rn"] = {
+					"o<CR># %%<CR>",
+					desc = "New 'cell'",
 				},
-
-				-- tables with just a `desc` key will be registered with which-key if it's installed
-				-- this is useful for naming menus
-				-- ["<Leader>b"] = { desc = "Buffers" },
-
-				-- setting a mapping to false will disable it
-				-- ["<C-S>"] = false,
+				["]C"] = {
+					[[:/^\# %%<CR>j]],
+					desc = "Go to next cell",
+				},
+				["[C"] = {
+					[[:?^\# %%<CR>?^\# %%<CR>j]],
+					desc = "Go to previous cell",
+				},
+				["<Leader>rm"] = {
+					"o<CR># %% [markdown]<CR># ",
+					desc = "New Markdown 'cell'",
+				},
+				["<Leader>Sz"] = {
+					function()
+						local stats = require("lazy").stats()
+						local ms = math.floor(require("lazy").stats().startuptime * 100 + 0.5) / 100
+						local stat_string = "loaded "
+								.. stats.loaded
+								.. "/"
+								.. stats.count
+								.. " plugins in "
+								.. ms
+								.. "ms"
+						vim.api.nvim_echo({ { stat_string, "Title" } }, true, {})
+					end,
+					desc = "Show Lazy loading stats",
+				},
 			},
+			i = {
+				-- ["<C-K>"] = { function() require("luasnip").expand() end, silent = true },
+				-- ["<C-L>"] = { function() require("luasnip").jump(1) end, silent = true },
+				-- ["<C-J>"] = { function() require("luasnip").jump(-1) end, silent = true },
+				-- ["<C-E>"] = {
+				--   function()
+				--     if require("luasnip").choice_active() then require("luasnip").change_choice(1) end
+				--   end,
+				--   silent = true,
+				-- },
+			},
+			x = {},
+			o = {},
+			t = {},
 		},
 	},
 }
