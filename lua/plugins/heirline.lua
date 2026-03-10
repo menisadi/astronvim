01dd9f8fe1d534d8f58acb54d26572836485fca9
sadigu <menis@intentiq.com>
2026-03-10 14:54:31 +0200

    Customizing heriline to look better (imo)
    Mode Text is always visible
    Right-side mode indicator removed

---
+++ lua/plugins/heirline.lua
@@ -0,0 +1,62 @@
+return {
+  "rebelot/heirline.nvim",
+  opts = function(_, opts)
+    local status = require("astroui.status")
+    opts.statusline = {
+      hl = { fg = "fg", bg = "bg" },
+      status.component.mode({
+        mode_text = { padding = { left = 1, right = 1 } },
+      }),
+      { flexible = 3, status.component.git_branch(), {} },
+      { flexible = 2, status.component.file_info(),  {} },
+      { flexible = 4, status.component.git_diff(),   {} },
+      status.component.diagnostics(),
+      status.component.fill(),
+      status.component.cmd_info(),
+      { flexible = 1, status.component.fill(), {} },
+      status.component.lsp(),
+      status.component.virtual_env(),
+      status.component.treesitter(),
+      {
+        static = {
+          n_requests = 0,
+          spinner_index = 0,
+          spinner_symbols = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+          done_symbol = "✓",
+        },
+        init = function(self)
+          if self._cc_autocmds then return end
+          self._cc_autocmds = true
+          vim.api.nvim_create_autocmd("User", {
+            pattern = "CodeCompanionRequestStarted",
+            callback = function()
+              self.n_requests = self.n_requests + 1
+              vim.cmd("redrawstatus")
+            end,
+          })
+          vim.api.nvim_create_autocmd("User", {
+            pattern = "CodeCompanionRequestFinished",
+            callback = function()
+              self.n_requests = math.max(0, self.n_requests - 1)
+              vim.cmd("redrawstatus")
+            end,
+          })
+        end,
+        provider = function(self)
+          if not package.loaded["codecompanion"] then return nil end
+          local symbol
+          if self.n_requests > 0 then
+            self.spinner_index = (self.spinner_index % #self.spinner_symbols) + 1
+            symbol = self.spinner_symbols[self.spinner_index]
+          else
+            symbol = self.done_symbol
+            self.spinner_index = 0
+          end
+          return (" %d %s "):format(self.n_requests, symbol)
+        end,
+        hl = function() return status.hl.filetype_color() end,
+      },
+      status.component.nav(),
+    }
+  end,
+}
